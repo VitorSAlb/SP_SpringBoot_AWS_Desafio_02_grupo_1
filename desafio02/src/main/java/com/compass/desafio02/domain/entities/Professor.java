e88e4992edb38fd6f53a303beff13beec6c0426b
Vitor Albuquerque <vitor.s.albq@gmail.com>
2024-10-29 09:45:35 -0300

    feat: Initial architecture

---
+++ desafio02/src/main/java/com/compass/desafio02/domain/entities/Professor.java
@@ -0,0 +1,4 @@
+package com.compass.desafio02.domain.entities;
+
+public class Professor {
+}
---
+++ desafio02/src/main/java/com/compass/desafio02/domain/repositories/StudentRepository.java
@@ -0,0 +1,4 @@
+package com.compass.desafio02.domain.repositories;
+
+public class StudentRepository {
+}
