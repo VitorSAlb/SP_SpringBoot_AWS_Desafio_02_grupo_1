8043347c654f2b00655aeec34638462167f055b5
Vitor Albuquerque <vitor.s.albq@gmail.com>
2024-10-30 08:20:31 -0300

    feat: All repositories

--- desafio02/src/main/java/com/compass/desafio02/domain/repositories/ProfessorRepository.java
+++ desafio02/src/main/java/com/compass/desafio02/domain/repositories/ProfessorRepository.java
@@ -1,4 +1,7 @@
 package com.compass.desafio02.domain.repositories;
 
-public class ProfessorRepository {
+import com.compass.desafio02.domain.entities.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfessorRepository extends JpaRepository<Professor, Integer> {
 }
