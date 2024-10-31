0b58854bfb8ad84f92a5eb72a748b116490e1777
Victor Hugo <victorcordeirojob@gmail.com>
2024-10-31 17:51:37 -0300

    :bug: fix: Bug in data insertion

--- desafio02/src/main/java/com/compass/desafio02/domain/services/ProfessorService.java
+++ desafio02/src/main/java/com/compass/desafio02/domain/services/ProfessorService.java
@@ -1,14 +1,17 @@
 package com.compass.desafio02.domain.services;
 
 import com.compass.desafio02.domain.entities.Professor;
-import com.compass.desafio02.domain.entities.Student;
 import com.compass.desafio02.domain.repositories.ProfessorRepository;
+import com.compass.desafio02.domain.repositories.SubjectRepository;
 import com.compass.desafio02.domain.repositories.projection.ProfessorProjection;
-import com.compass.desafio02.domain.repositories.projection.StudentProjection;
+import com.compass.desafio02.domain.entities.Subject;
+import com.compass.desafio02.infrastructure.exceptions.BusinessRuleException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProfessorService {
@@ -16,13 +19,51 @@ public class ProfessorService {
     @Autowired
     private ProfessorRepository professorRepository;
 
+    @Autowired
+    private SubjectRepository subjectRepository;
+
+    public void validateProfessorSubjects(Professor professor) {
+        List<Subject> subjectsAsMain = subjectRepository.findByMainProfessor(professor);
+        List<Subject> subjectsAsSubstitute = subjectRepository.findBySubstituteProfessor(professor);
+
+        int totalSubjects = subjectsAsMain.size() + subjectsAsSubstitute.size();
+
+        if (totalSubjects > 3) {
+            throw new BusinessRuleException("A professor can participate in a maximum of 3 subjects.");
+        }
+
+        if (subjectsAsMain.size() > 1) {
+            throw new BusinessRuleException("A professor can only be a main professor in one subject.");
+        }
+
+        long substituteInSameCourse = subjectsAsSubstitute.stream()
+                .filter(subject -> subject.getCourse().equals(professor.getCourse()))
+                .count();
+
+        if (substituteInSameCourse > 1) {
+            throw new BusinessRuleException("A professor can only be a substitute in one subject within their own course.");
+        }
+
+        long substituteInOtherCourses = subjectsAsSubstitute.stream()
+                .filter(subject -> !subject.getCourse().equals(professor.getCourse()))
+                .count();
+
+        if (substituteInOtherCourses > 1) {
+            throw new BusinessRuleException("A professor can only be a substitute in one subject of another course.");
+        }
+    }
+
     public Professor save(Professor professor) {
-        return professorRepository.save(professor);
+        Professor savedProfessor = professorRepository.save(professor);
+
+        validateProfessorSubjects(savedProfessor);
+
+        return savedProfessor;
     }
 
     public Professor findById(Integer id) {
         return professorRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("Professor not founded") // TROCAR EXCESSÃO
+                () -> new RuntimeException("Professor not found") // CHANGE EXCEPTION
         );
     }
 
@@ -30,7 +71,7 @@ public class ProfessorService {
         try {
             return professorRepository.findByEmail(email);
         } catch (RuntimeException e) {
-            throw new RuntimeException("Email not founded"); // TROCAR EXCESSÃO
+            throw new RuntimeException("Email not found"); // CHANGE EXCEPTION
         }
     }
 
@@ -41,7 +82,7 @@ public class ProfessorService {
     public Professor update(Integer id, Professor newProfessor) {
         Professor existingProfessor = findById(id);
 
-        existingProfessor.setEmail(newProfessor.getEmail()); // TESMOS QUE VERIFICAR SE O NOVO EMAIL JÁ EXISTE PARA PODER EDITAR
+        existingProfessor.setEmail(newProfessor.getEmail());
         existingProfessor.setFirstName(newProfessor.getFirstName());
         existingProfessor.setLastName(newProfessor.getLastName());
         existingProfessor.setBirthdate(newProfessor.getBirthdate());
