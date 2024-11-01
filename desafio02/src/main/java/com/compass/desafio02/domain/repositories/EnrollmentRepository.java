6de85ad1d3a18e10ee5c023fae179ae0b4fa60b1
Carlos Renan <carlosrenangama41@gmail.com>
2024-11-01 13:49:38 -0300

    feat: Add methods in EnrollmentRepository to find enrollments by course ID and student ID

--- desafio02/src/main/java/com/compass/desafio02/domain/repositories/EnrollmentRepository.java
+++ desafio02/src/main/java/com/compass/desafio02/domain/repositories/EnrollmentRepository.java
@@ -5,9 +5,15 @@ import com.compass.desafio02.domain.entities.Enrollment;
 import com.compass.desafio02.domain.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Integer> {
 
     boolean existsByStudentAndCourse(Student student, Course course);
 
     boolean existsByStudentId(Integer studentId);
+
+    List<Enrollment> findByCourseId(Integer courseId);
+
+    List<Enrollment> findByStudentId(Integer studentId);
 }
